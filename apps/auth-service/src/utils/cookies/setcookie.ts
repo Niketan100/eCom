420f10dad26bb0da385dcae7183d70b57051ffdd
Niketan100 <niketankumar12345@gmail.com>
2026-07-14 13:41:57 +0530

    CookieSet

--- apps/auth-service/src/utils/cookies/setcookie.ts
+++ apps/auth-service/src/utils/cookies/setcookie.ts
@@ -1,10 +1,10 @@
 import { Response } from "express";
 
 export const setCookie = (res : Response , name :string , value : string ) => { 
-     res.cookie(name, value, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        res.cookie(name, value, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 }  
